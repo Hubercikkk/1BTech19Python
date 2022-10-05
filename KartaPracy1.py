087ebfd49ee3f020b4520580b3f2965755d087af
1B11HK <114583596+Hubercikkk@users.noreply.github.com>
2022-10-05 12:13:15 +0000

    skonczone zad 6

--- KartaPracy1.py
+++ KartaPracy1.py
@@ -9,3 +9,21 @@ print(a**2 + b**2)
 a = int(input())
 b = int(input())
 print((a+b)**2)
+
+# zad 3
+
+a = int(input())
+b = int(input())
+# print((a-b)**3)
+
+# zad 4
+# a, b, c = map(int, input().split)
+#print(a*b*c)
+
+# zad 5
+a, b = int(input()), int(input())
+# print(2*(a+b)/5)
+
+# zad 6
+brutto = float(input())
+print(brutto / 1.23)
